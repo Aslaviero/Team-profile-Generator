98d9492e52f5772ecca9df2be86ad2ba8f38d058
Anthony Slaviero <tonyslaviero@gmail.com>
2022-08-30 22:00:06 -0700

    Updated readme and built page in html

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const inquirer = require("inquirer");
+const fs = require('fs');
+const app = express;
+// const PORT = 5000;
+// const path = require('path')
+const Engineer = ('./lib/Employee.js');
+const Intern = ('./lib/Intern.js');
+const Employee = ('./lib/Employee.js');
+const Manager = ('./lib/Manager.js');
+
