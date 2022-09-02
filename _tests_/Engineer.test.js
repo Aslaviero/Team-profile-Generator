68d752a4121deea10680a24f4dde7c9514eaff91
Anthony Slaviero <tonyslaviero@gmail.com>
2022-09-01 18:32:53 -0700

    Got questions working

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,19 +1,21 @@
-const Engineer = require("../lib/Engineer");
 
-test("Can create a github.", () => {
-    const testGithub = "Leia Organa";
-    const employeeInstance = new Engineer("Leia Organa", 2, "princessleia@starwars.com", testGithub);
-    expect(employeeInstance.github).toBe(testGithub);
-});
+const { Engineer } = require('../lib/Engineer');
 
-test("Testing getGithub will return github.", () => {
-    const testGithub = "Leia Organa";
-    const employeeInstance = new Engineer("Leia Organa", 2, "princessleia@starwars.com", testGithub);
-    expect(employeeInstance.getGithub()).toBe(testGithub);
-});
+test('Creates new Engineer', () => {
+  const employee = new Engineer('John Smith', 1234, 'john@smith.com', 'john-smith');
 
-test("Testing role.", () => {
-    const returnValue = "Engineer";
-    const employeeInstance = new Engineer("Leia Organa", 2, "princessleia@starwars.com", "Leia Organa");
-    expect(employeeInstance.getRole()).toBe(returnValue);
-});
+  expect(employee.name).toBe("John Smith");
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+  expect(employee.github).toEqual(expect.any(String));
+})
+
+test('Checks all methods for Engineer class', () => {
+  const employee = new Engineer('John Smith', 1234, 'john@smith.com', 'john-smith');
+
+  expect(employee.getName()).toBe(employee.name);
+  expect(employee.getId()).toBe(employee.id);
+  expect(employee.getEmail()).toBe(employee.email);
+  expect(employee.getGithub()).toBe(employee.github);
+  expect(employee.getRole()).toBe('Engineer');
+})
